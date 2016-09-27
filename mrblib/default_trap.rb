c800f590db9594e57c4899af0dbeb08d299e7bd1
ksss <co000ri@gmail.com>
2016-09-27 16:42:12 +0900

    Set default trap functions same as CRuby

--- mrblib/default_trap.rb
+++ mrblib/default_trap.rb
@@ -2,5 +2,7 @@ Signal.list.each do |sig, no|
   case sig
   when "INT"
     Signal.trap(sig) { raise Interrupt, "" }
+  when "HUP", "TERM", "QUIT", "ALRM", "USR1", "USR2"
+    Signal.trap(sig) { raise SignalException.new("SIG#{sig}", no) }
   end
 end
