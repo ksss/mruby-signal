89ec801d1757667f0a3256a91fc3c1b4359549df
ksss <co000ri@gmail.com>
2016-10-03 11:16:47 +0900

    Add test

---
+++ .travis-test-build_config.rb
@@ -0,0 +1,16 @@
+MRuby::Build.new do |conf|
+  toolchain :gcc
+  conf.gem :core => "mruby-bin-mruby"
+  conf.gem :core => "mruby-print"
+end
+
+MRuby::Build.new('test') do |conf|
+  toolchain :gcc
+
+  enable_debug
+  conf.enable_bintest
+  conf.enable_test
+
+  conf.gem :core => "mruby-print"
+  conf.gem "../../mruby-signal"
+end
